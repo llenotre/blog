4f26a87203c7bb47bd389ac41bd935abe4f56f9e
llenotre <llenotre@student.42.fr>
2023-06-03 01:21:04 +0200

    Newsletter WIP

--- src/newsletter.rs
+++ src/newsletter.rs
@@ -6,21 +6,24 @@ use actix_web::error;
 use actix_web::post;
 use actix_web::web;
 use bson::doc;
+use bson::oid::ObjectId;
 use chrono::DateTime;
 use chrono::Utc;
 use crate::GlobalData;
 use crate::util;
-use mongodb::options::ReplaceOptions;
 use serde::Deserialize;
 use serde::Serialize;
 
 /// An email address of a newsletter subscriber.
 #[derive(Deserialize, Serialize)]
 pub struct NewsletterEmail {
-	pub email: String,
+	/// The registered email. If `None`, the email has been anonymized.
+	pub email: Option<String>,
 	/// The date at which the user subscribed.
 	#[serde(with = "util::serde_date_time")]
 	pub date: DateTime<Utc>,
+	/// Tells whether the user has unsubscribed.
+	pub unsubscribed: bool,
 }
 
 /// The state of a message.
@@ -29,7 +32,9 @@ pub enum NewsletterMessageState {
 	/// The message is waiting to be sent.
 	Pending,
 	/// Message could not be delivered.
-	Failed,
+	Failed {
+		reason: String,
+	},
 	/// The message has been sent successfuly.
 	Sent {
 		/// The date at which the message has been sent.
@@ -41,6 +46,9 @@ pub enum NewsletterMessageState {
 /// An email message either sent or to be sent.
 #[derive(Deserialize, Serialize)]
 pub struct NewsletterMessage {
+	/// The ID of the template associated with the message.
+	pub template_id: ObjectId,
+
 	/// Source email.
 	pub from: String,
 	/// Destination email.
@@ -50,6 +58,17 @@ pub struct NewsletterMessage {
 
 	/// The state of the message.
 	pub state: NewsletterMessageState,
+}
+
+/// An email template to be sent.
+#[derive(Deserialize, Serialize)]
+pub struct NewsletterTemplate {
+	/// The name of the template.
+	pub name: String,
+	/// The number of emails to send per second.
+	pub send_speed: u32,
+	/// The template to build emails.
+	pub template: String,
 }
 
 /// Payload of request to register a newsletter subscriber.
@@ -71,15 +90,13 @@ pub async fn subscribe(
 
 	let db = data.get_database();
 	db.collection("newsletter_subscriber")
-		.replace_one(
-			doc! {
-				"email": &info.email,
-			},
+		.insert_one(
 			NewsletterEmail {
-				email: info.email,
+				email: Some(info.email),
 				date: chrono::offset::Utc::now(),
+				unsubscribed: false,
 			},
-			Some(ReplaceOptions::builder().upsert(true).build())
+			None
 		)
 		.await
 		.map_err(|_| error::ErrorInternalServerError(""))?;
