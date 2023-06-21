7116c49e7fd169754ce4181a74176601a5c19763
llenotre <llenotre@student.42.fr>
2023-06-21 23:05:27 +0200

    Email sending WIP

--- src/newsletter.rs
+++ src/newsletter.rs
@@ -1,16 +1,19 @@
 //! TODO doc
 
-use crate::util;
-use crate::GlobalData;
+use lettre::Message;
+use futures_util::TryStreamExt;
+use bson::Bson;
+use actix_web::HttpResponse;
+use actix_web::Responder;
 use actix_web::error;
 use actix_web::post;
 use actix_web::web;
-use actix_web::HttpResponse;
-use actix_web::Responder;
 use bson::doc;
 use bson::oid::ObjectId;
 use chrono::DateTime;
 use chrono::Utc;
+use crate::GlobalData;
+use crate::util;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -39,6 +42,8 @@ pub enum NewsletterMessageState {
 		#[serde(with = "util::serde_date_time")]
 		date: DateTime<Utc>,
 	},
+    /// The email has been cancelled.
+    Cancelled,
 }
 
 /// An email message either sent or to be sent.
@@ -47,12 +52,10 @@ pub struct NewsletterMessage {
 	/// The ID of the template associated with the message.
 	pub template_id: ObjectId,
 
-	/// Source email.
-	pub from: String,
-	/// Destination email.
-	pub to: String,
+	/// Destination email. If `None`, the email has been anonymized.
+	pub to: Option<String>,
 	/// The email's content.
-	pub content: String,
+	pub content: Vec<u8>,
 
 	/// The state of the message.
 	pub state: NewsletterMessageState,
@@ -61,12 +64,84 @@ pub struct NewsletterMessage {
 /// An email template to be sent.
 #[derive(Deserialize, Serialize)]
 pub struct NewsletterTemplate {
+	/// The template's id.
+	#[serde(rename = "_id")]
+	pub id: ObjectId,
+
 	/// The name of the template.
 	pub name: String,
 	/// The number of emails to send per second.
 	pub send_speed: u32,
-	/// The template to build emails.
-	pub template: String,
+
+	/// The email's title.
+	pub title: String,
+	/// The content template.
+	pub content_template: String,
+}
+
+/// Component scheduling email sending.
+pub struct EmailWorker {
+    /// Global data, containing database client.
+    data: web::Data<GlobalData>,
+}
+
+impl EmailWorker {
+    /// Creates a new instance.
+    pub fn new(data: web::Data<GlobalData>) -> Self {
+        Self {
+            data,
+        }
+    }
+
+    /// Runs the worker.
+    pub async fn run(&self) {
+        // TODO
+    }
+
+    /// Sends a mail.
+    pub async fn enqueue(&self, msg: NewsletterMessage) -> Result<(), mongodb::error::Error> {
+        let coll = self.data.get_database().collection("newsletter_message");
+        coll.insert_one(msg, None).await.map(|_| ())
+    }
+
+    /// Launches an email campain on all registered emails.
+    pub async fn launch_campain(&self, template: &NewsletterTemplate) -> Result<(), mongodb::error::Error> {
+        let subscribers_coll = self.data.get_database().collection::<NewsletterEmail>("newsletter_subscriber");
+        let mut stream = subscribers_coll.find(doc! {
+            "email": {"$ne": Bson::Null },
+            "unsubscribed": false
+        }, None)
+            .await?;
+
+        // TODO implement errors handling, retrying on reboot, etc...
+        while let Some(email) = stream.try_next().await? {
+            let Some(email) = email.email else {
+                continue;
+            };
+
+            // Build mail
+            let message = Message::builder()
+                .from("newsletter <newsletter@blog.lenot.re>".parse().unwrap())
+                .to(email.parse().unwrap())
+                .subject(template.title.clone())
+                // TODO fill template with unsubscribe URL
+                .body(template.content_template.clone())
+                .unwrap();
+            // TODO sign message (DKIM)
+
+            // Enqueue mail
+            self.enqueue(NewsletterMessage {
+                template_id: ObjectId::new(),
+
+                to: Some(email),
+                content: message.formatted(),
+
+                state: NewsletterMessageState::Pending,
+            }).await?;
+        }
+
+        Ok(())
+    }
 }
 
 /// Payload of request to register a newsletter subscriber.
