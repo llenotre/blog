28df26d0e949a146a530a4d5e1f5f04b18ff1068
llenotre <llenotre@student.42.fr>
2023-06-25 19:45:17 +0200

    Article mobile display fix

--- src/user.rs
+++ src/user.rs
@@ -25,7 +25,7 @@ pub fn get_auth_url(client_id: &str) -> String {
 pub fn redirect_to_last_article(session: &Session) -> Redirect {
 	let last_article = session.get::<String>("last_article");
 	let uri = match last_article {
-		Ok(Some(last_article)) => format!("/article/{}", last_article),
+		Ok(Some(last_article)) => format!("/article/{}/", last_article),
 		_ => "/".to_owned(),
 	};
 
