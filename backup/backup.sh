e5e8e4f1bbaeced00ff8cb13f8bc4734769ce0ae
llenotre <llenotre@student.42.fr>
2023-07-17 22:19:20 +0200

    Fixing backups and remove mongo-express because of CVE

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
+
+set -e
  
 TIMESTAMP=`date --iso-8601=seconds`
-DBNAME="blog"
 
 mkdir -p backup/
 cd backup/
 
-# Lock DB
-mongo admin --eval "printjson(db.fsyncLock())"
-mongodump --username $MONGO_ROOT_USERNAME --password $MONGO_ROOT_PASSWORD -d $DBNAME mongodb://mongo:27017
-# Unlock DB
-mongo admin --eval "printjson(db.fsyncUnlock())"
+echo "Performing backup $TIMESTAMP..."
+
+mongodump --uri="mongodb://$MONGO_ROOT_USERNAME:$MONGO_ROOT_PASSWORD@mongo:27017" --oplog
+
+echo "Mongodb dump: OK"
  
 # Add timestamp to backup
 mv dump $TIMESTAMP
 tar cf $TIMESTAMP.tar mongodb-$TIMESTAMP
 gzip $TIMESTAMP.tar
+
+echo "Compression: OK"
  
 # Upload
-aws s3 cp $TIMESTAMP.tar.gz s3://maestro/backup/mongodb/$DBNAME
- 
+aws s3 cp $TIMESTAMP.tar.gz s3://maestro/backup/mongodb/
+
+echo "Upload: OK"
 
 # Cleanup
 cd ..
 rm -rf backup/
+
+echo "Done!"
