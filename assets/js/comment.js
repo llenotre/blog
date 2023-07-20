5a5b3295306e581e58e524538f2024fcfc07af93
llenotre <llenotre@student.42.fr>
2023-07-20 22:16:57 +0200

    fixing comment post

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -89,69 +89,80 @@ function expand_editor(id) {
 }
 
 // Posts a comment.
-function post(comment_id) {
+async function post(comment_id) {
 	var comment_content = document.getElementById("comment-" + comment_id + "-post-content");
 	if (comment_content.value.length == 0) {
 		return;
 	}
 
     // Post comment
-	var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("POST", "/comment", false);
-    xmlHttp.setRequestHeader("Content-Type", "application/json");
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
 	var payload = JSON.stringify({
 		"article_id": article_id,
 		"response_to": comment_id,
 		"content": comment_content.value
 	});
-    xmlHttp.send(payload);
-	if (xmlHttp.status != 200) {
-		alert("Failed to post comment:" + xmlHttp.responseText);
-		return;
-	}
-	var result = JSON.parse(xmlHttp.responseText);
-
-    // Get new comment's HTML
-	var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "/comment/" + result["id"], false);
-    xmlHttp.send(null);
-	if (xmlHttp.status != 200) {
-		alert("Failed to post comment:" + xmlHttp.responseText);
-		return;
-	}
-	var content = xmlHttp.responseText;
+	var id = await fetch("/comment", { method: "POST", headers: headers, body: payload })
+		.then(async function(response) {
+			if (response.status == 200) {
+				var json = await response.json();
+				return json["id"];
+			} else {
+				var error = await response.text();
+				alert("Failed to post comment:" + error);
+				return null;
+			}
+		});
+	// Get new comment's HTML
+	var comment_html = await fetch("/comment/" + id)
+		.then(async function(response) {
+			var body = await response.text();
+			if (response.status == 200) {
+				return body;
+			} else {
+				alert("Failed to post comment:" + body);
+				return null;
+			}
+		});
 
     // Add comment on front-end
+	// TODO if reply to another comment, select the appropriate div
     var comments_list = document.getElementById("comments-list");
-    comments_list.innerHTML += content;
+    comments_list.innerHTML += comment_html;
+
+	// Format comment's date
+	var com = document.getElementById("com-" + id);
+	format_date_long(com.querySelector("#date-long"));
 
     // Empty text editor
     comment_content.value = "";
-    input(comment_id);
+    input(comment_id, "post");
 
     // Update comments count
     var coms_count = document.getElementById("comments-count");
-    coms_count.textContent += 1;
+    coms_count.textContent = parseInt(coms_count.textContent) + 1;
 }
 
 // Edits the comment with the given ID.
-function edit(comment_id) {
+async function edit(comment_id) {
 	var comment_content = document.getElementById("comment-" + comment_id + "-edit-content");
 	if (comment_content.value.length == 0) {
 		return;
 	}
 
     // Update comment
-	var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("PATCH", "/comment", false);
-    xmlHttp.setRequestHeader("Content-Type", "application/json");
+	var headers = new Headers();
+    headers.append("Content-Type", "application/json");
 	var payload = JSON.stringify({
 		"comment_id": comment_id,
 		"content": comment_content.value
 	});
-    xmlHttp.send(payload);
-	if (xmlHttp.status != 200) {
-		alert("Failed to edit comment" + xmlHttp.responseText);
+	var response = await fetch("/comment", { method: "PATCH", headers: headers, body: payload });
+	if (response.status != 200) {
+		var error = await response.text();
+		alert("Failed to edit comment" + error);
+		return;
 	}
 
     // Update comment on front-end
@@ -159,16 +170,16 @@ function edit(comment_id) {
 }
 
 // Deletes the comment with the given ID.
-function del(comment_id) {
+async function del(comment_id) {
 	if (!confirm("Are you sure you want to delete this comment?")) {
 		return;
 	}
 
-	var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("DELETE", "/comment/" + comment_id, false);
-    xmlHttp.send(null);
-	if (xmlHttp.status != 200) {
-		alert("Failed to delete comment" + xmlHttp.responseText);
+	var response = await fetch("/comment/" + comment_id, { method: "DELETE" });
+	if (response.status != 200) {
+		var error = await response.text();
+		alert("Failed to delete comment" + error);
+		return;
 	}
 
     // Remove comment from front-end
--- assets/js/date.js
+++ assets/js/date.js
@@ -1,6 +1,8 @@
+function format_date_long(e) {
+    e.innerHTML = dayjs(e.innerHTML).format('HH:mm, dddd MMMM D, YYYY');
+}
+
 document.querySelectorAll('[id=date]').forEach(e => {
     e.innerHTML = dayjs(e.innerHTML).format('dddd MMMM D, YYYY');
 });
-document.querySelectorAll('[id=date-long]').forEach(e => {
-    e.innerHTML = dayjs(e.innerHTML).format('HH:mm, dddd MMMM D, YYYY');
-});
+document.querySelectorAll('[id=date-long]').forEach(e => format_date_long(e));
