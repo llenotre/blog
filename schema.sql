7a49b9f4578e9f128ba7c9d3df0063405c5da20a
llenotre <llenotre@student.42.fr>
2023-10-20 19:00:00 +0200

    fix: schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS article (
     id INT PRIMARY KEY NOT NULL,
     post_date TIMESTAMP,
-    content_id INT NOT NULL,
+    content_id INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS article_content (
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS article_content (
     article_id INT NOT NULL,
     edit_date TIMESTAMP NOT NULL,
     title TEXT NOT NULL,
-    desc TEXT NOT NULL,
+    description TEXT NOT NULL,
     cover_url TEXT NOT NULL,
     content TEXT NOT NULL,
     tags TEXT NOT NULL,
     public BOOLEAN NOT NULL,
     sponsors BOOLEAN NOT NULL,
-    comments_locked NOT NULL,
+    comments_locked BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS file (
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS file (
     name TEXT,
     mime_type TEXT NOT NULL,
     upload_date TIMESTAMP NOT NULL,
-    data BYTEA NOT NULL,
+    data BYTEA NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS analytics (
@@ -34,16 +34,16 @@ CREATE TABLE IF NOT EXISTS analytics (
     geolocation JSON,
     device JSON,
     method VARCHAR(16) NOT NULL,
-    uri VARCHAR(255) NOT NULL,
+    uri VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS newsletter_subscriber (
     id INT PRIMARY KEY NOT NULL,
     email TEXT,
-    subscribe_date TIMESTAMP NOT NULL,
+    subscribe_date TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS "user" (
     id INT PRIMARY KEY NOT NULL,
     access_token TEXT NOT NULL,
     github_login TEXT NOT NULL,
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS user (
     admin BOOLEAN NOT NULL,
     banned BOOLEAN NOT NULL,
     register_date TIMESTAMP NOT NULL,
-    last_post TIMESTAMP NOT NULL,
+    last_post TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comment (
@@ -62,11 +62,11 @@ CREATE TABLE IF NOT EXISTS comment (
     author_id INT NOT NULL,
     post_date TIMESTAMP NOT NULL,
     content_id INT NOT NULL,
-    removed TIMESTAMP,
+    removed TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS comment_content (
     id INT PRIMARY KEY NOT NULL,
     edit_date TIMESTAMP NOT NULL,
-    content TEXT NOT NULL,
+    content TEXT NOT NULL
 );
